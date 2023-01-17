c6d41c7dfe580ee578a03e1aac0878c8ca90cc16
dwycoco <dwycoco@gmail.com>
2023-01-17 18:53:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function agechecker() {
+let age = prompt('what is your age?');
+   if (age <=18) {
+      alert('this person is underage')
+
+   } else {
+      alert('this person can buy liquor')
+   }
+}
